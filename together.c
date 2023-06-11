5a907fe21de57646391a30c5133266d7d198c755
B0cz3k <lukasz.borak03@gmail.com>
2023-06-11 21:10:16 +0200

    tweaks

--- together.c
+++ together.c
@@ -25,18 +25,15 @@ void* producer(void* arg) {
         
         pthread_mutex_lock(&mutex);
 
-        while (buffer_index == BUFFER_SIZE) { // check if the buffer is full
+        while(buffer_index == BUFFER_SIZE) { // check if the buffer is full
             pthread_cond_wait(&buffer_not_full, &mutex);
         }
-
         buffer[buffer_index++] = 'O';
         printf("Producer %d:\t%s\n", producer_id, buffer);
 
         pthread_cond_signal(&buffer_not_empty);
-
         pthread_mutex_unlock(&mutex);
     }
-
     pthread_exit(NULL);
 }
 
@@ -49,18 +46,15 @@ void* consumer(void* arg) {
 
         pthread_mutex_lock(&mutex);
 
-        while (buffer_index == 0) { // check if the buffer is empty
+        while(buffer_index == 0) { // check if the buffer is empty
             pthread_cond_wait(&buffer_not_empty, &mutex);
         }
-
         buffer[--buffer_index] = 'X';
         printf("Consumer %d:\t%s\n", consumer_id, buffer);
 
         pthread_cond_signal(&buffer_not_full);
-
         pthread_mutex_unlock(&mutex);
     }
-
     pthread_exit(NULL);
 }
 
@@ -72,23 +66,23 @@ int main() {
     pthread_cond_init(&buffer_not_full, NULL);
     pthread_cond_init(&buffer_not_empty, NULL);
 
-    for (int i = 0; i < NUM_PRODUCERS; i++) {
+    for(int i = 0; i < NUM_PRODUCERS; i++) {
         int* producer_id = malloc(sizeof(int));
         *producer_id = i + 1;
         pthread_create(&producer_threads[i], NULL, producer, (void*)producer_id);
     }
 
-    for (int i = 0; i < NUM_CONSUMERS; i++) {
+    for(int i = 0; i < NUM_CONSUMERS; i++) {
         int* consumer_id = malloc(sizeof(int));
         *consumer_id = i + 1;
         pthread_create(&consumer_threads[i], NULL, consumer, (void*)consumer_id);
     }
 
-    for (int i = 0; i < NUM_PRODUCERS; i++) {
+    for(int i = 0; i < NUM_PRODUCERS; i++) {
         pthread_join(producer_threads[i], NULL);
     }
 
-    for (int i = 0; i < NUM_CONSUMERS; i++) {
+    for(int i = 0; i < NUM_CONSUMERS; i++) {
         pthread_join(consumer_threads[i], NULL);
     }
 
